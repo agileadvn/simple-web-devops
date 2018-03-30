404701ecf1d6f6d1895a705dae8e304723b0f65f
Hien Nguyen <hien@planday.com>
2018-03-31 01:17:03 +0700

    Case of failure

--- db/scripts.sql
+++ db/scripts.sql
@@ -1,8 +1,8 @@
 create table products(
   id int,
-  name varchar(20),
+  name varchar(50),
   description varchar(100)
-)
+);
 
-insert into products values(1001, 'iPhone X', 'The best phone ever');
-insert into products values(1002, 'Samsung Galaxy Note 8', 'The phone of new world');
+insert into products values(1001, N'iPhone X', N'The best phone ever');
+insert into products values(1002, N'Samsung Galaxy Note 8', N'The phone of new world');
